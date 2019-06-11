229b2879d4d5b846b1068d46699dec1221a42f77
mrkchoi <kennethichoi@gmail.com>
2019-06-11 12:24:50 -0700

    Implement Subs + Posts controller

---
+++ RedditClone/app/controllers/posts_controller.rb
@@ -0,0 +1,24 @@
+class PostsController < ApplicationController
+
+  def new
+  end
+  
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+  def post_params
+  end
+end
---
+++ RedditClone/app/controllers/subs_controller.rb
@@ -0,0 +1,48 @@
+class SubsController < ApplicationController
+  
+  def new
+    render :new
+  end
+  
+  def create
+    @sub = Sub.new(sub_params)
+    
+    if @sub.save
+      redirect_to sub_url(@sub)
+    else
+      flash.now[:errors] = @sub.errors.full_messages
+      render :new
+    end
+  end
+
+  def index
+    @subs = Sub.all
+    render :index
+  end
+
+  def show
+    @sub = Sub.find(params[:id])
+    render :show
+  end
+
+  def edit
+    @sub = Sub.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @sub = Sub.find(params[:id])
+
+    if @sub.moderator_id == current_user.id && @sub.update_attributes(sub_params)
+      redirect_to sub_url(@sub)
+    else
+      flash.now[:errors] = @sub.errors.full_messages
+      render :edit
+    end
+  end
+
+  private
+  def sub_params
+    params.require(:sub).permit(:title, :description, :moderator_id)
+  end
+end
